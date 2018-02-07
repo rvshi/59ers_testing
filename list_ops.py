59ba0191019c45077838320499a5c0db8f2aa613
Harvey Shi <harveyyshi@gmail.com>
2018-02-07 16:14:48 -0500

    Updates files to conform to PEP8.

--- list_ops.py
+++ list_ops.py
@@ -1,8 +1,13 @@
 def get_sum(input_list):
+    '''
+    Returns the sum of a list
+    '''
     return sum(input_list)
+
 
 def get_min_max(input_list):
     pass
+
 
 def get_max_diff(input_list):
     pass
