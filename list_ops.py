944ce8cc354a17bd4d4d4a5abc154a4495a20d5e
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-02-07 15:58:13 -0500

    Add max diff functionality

--- list_ops.py
+++ list_ops.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def get_sum(input_list):
     pass
 
@@ -5,4 +7,6 @@ def get_min_max(input_list):
     pass
 
 def get_max_diff(input_list):
-    pass
+    diff_arr = np.diff(input_list)
+    max_diff = np.max(diff_arr)
+    return max_diff
