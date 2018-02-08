f1c27028023353290954f854fe4df2c96b24cd14
Edward Liang <ed.l.1324@gmail.com>
2018-02-07 21:27:47 -0500

    Edl13/maxdiff (#5)
    * Add max diff functionality
    
    * Add testing for max diff module
    
    * Fixed additional whitespace issue for PEP8 compliance

--- list_ops.py
+++ list_ops.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def get_sum(input_list):
     '''
     Returns the sum of a list
@@ -10,4 +13,6 @@ def get_min_max(input_list):
 
 
 def get_max_diff(input_list):
-    pass
+    diff_arr = np.diff(input_list)
+    max_diff = np.max(diff_arr)
+    return max_diff
