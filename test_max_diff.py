8093278552506947e940c1de444e4ac37ff9ae7c
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-02-09 12:34:11 -0500

    Added float case in max_diff test

--- test_max_diff.py
+++ test_max_diff.py
@@ -1,6 +1,6 @@
 def test_max_diff():
     from list_ops import get_max_diff
-    input_list = ([1, 1, 3], [-1, 3, -16], [5, 5, 5, 5, 5])
-    output_results = (2, 4, 0)
+    input_list = ([1, 1, 3], [-1, 3, -16], [5, 5, 5, 5, 5], [3.4, 5, 6.3, 2])
+    output_results = (2, 4, 0, 1.6)
     for i, nums in enumerate(input_list):
         assert get_max_diff(nums) == output_results[i]
