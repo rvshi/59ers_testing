d0bdd8f640e480ae2029cf07d24547662c53ad57
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-02-11 18:58:33 -0500

    Added exception raising and testing for non-int inputs and missing numpy packages

--- list_ops.py
+++ list_ops.py
@@ -1,4 +1,8 @@
-import numpy as np
+try:
+    import numpy as np
+except ImportError:
+    raise ImportError('''Could not import numpy. Please make sure to have the
+                      package installed''')
 
 
 def get_sum(input_list):
@@ -9,10 +13,13 @@ def get_sum(input_list):
 
 
 def get_min_max(input_list):
-    pass
+    min_max = (np.amin(input_list), np.amax(input_list))
+    return min_max
 
 
 def get_max_diff(input_list):
+    if(not all([type(num) is int for num in input_list])):
+        raise TypeError('All inputs must be integers.')
     diff_arr = np.diff(input_list)
     max_diff = np.max(diff_arr)
     return max_diff
--- test_max_diff.py
+++ test_max_diff.py
@@ -1,6 +1,13 @@
+import pytest
+
+
 def test_max_diff():
     from list_ops import get_max_diff
-    input_list = ([1, 1, 3], [-1, 3, -16], [5, 5, 5, 5, 5], [3.4, 5, 6.3, 2])
-    output_results = (2, 4, 0, 1.6)
+    input_list = ([1, 1, 3], [-1, 3, -16], [5, 5, 5, 5, 5])
+    output_results = (2, 4, 0)
     for i, nums in enumerate(input_list):
         assert get_max_diff(nums) == output_results[i]
+    type_error_list = ([1, 2, 3.32], [4, 'GTHC', 2, 333])
+    for nums in type_error_list:
+        with pytest.raises(TypeError):
+            get_max_diff(nums)
