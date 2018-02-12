6c51b959787521565a149feb774965f777893a97
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-02-12 13:19:51 -0500

    Added separate input checking function and unit testing

--- list_ops.py
+++ list_ops.py
@@ -6,6 +6,7 @@ except ImportError:
 
 
 def get_sum(input_list):
+    check_inputs(input_list)
     '''
     Returns the sum of a list
     '''
@@ -13,6 +14,7 @@ def get_sum(input_list):
 
 
 def get_min_max(input_list):
+    check_inputs(input_list)
     min_max = (np.amin(input_list), np.amax(input_list))
     return min_max
 
@@ -29,12 +31,16 @@ def get_max_diff(input_list):
     :raise ValueError: All input elements must be between -9,000 and
     9,000
     '''
+    check_inputs(input_list)
+    diff_arr = np.diff(input_list)
+    max_diff = np.max(diff_arr)
+    return max_diff
+
+
+def check_inputs(input_list):
     if(not type(input_list) is list):
         raise TypeError('Input must list')
     if(not all([type(num) is int for num in input_list])):
         raise TypeError('All inputs in list must be integers.')
     if(any([abs(num) > 1000000 for num in input_list])):
         raise ValueError('All inputs must be between -9,000 and 9,000')
-    diff_arr = np.diff(input_list)
-    max_diff = np.max(diff_arr)
-    return max_diff
