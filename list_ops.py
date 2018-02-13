30bbd3f5674dd992768d22a3c3dcb478a7f79098
Michelle Wei <michellewei12345@gmail.com>
2018-02-12 23:33:20 -0500

    Squashed commit of the following:
    commit d5f8f2f6dcf2d258d5d868a7eba63c986ac421a0
    Author: Michelle Wei <michellewei12345@gmail.com>
    Date:   Mon Feb 12 23:26:55 2018 -0500
    
        Update min_max function to check inputs

--- list_ops.py
+++ list_ops.py
@@ -9,6 +9,14 @@ def get_sum(input_list):
 
 
 def get_min_max(input_list):
+    """ Returns min and max in a list
+
+    :param input_list: (int) list to get min and max of
+    :return: min and max of list in a tuple
+    :raises TypeError: Input must be lists
+    :raises TypeError: Input elements must be integers
+    :raises ValueError: All input elements must be between -9,000 and 9,000
+    """
     min_max = (np.amin(input_list), np.amax(input_list))
     return min_max
 
