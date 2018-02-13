4d138e85efd4bedc40beae21eda7fe004b99bb19
Michelle Wei <michellewei12345@gmail.com>
2018-02-12 23:42:18 -0500

    Fix merge conflict in list_ops.py

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Unit tests and CI assignment
 
 [![Build Status](https://travis-ci.org/Duke-BME-Design/59ers_testing.svg?branch=master)](https://travis-ci.org/Duke-BME-Design/59ers_testing)
+[![Documentation Status](http://readthedocs.org/projects/59ers-testing-and-documentation/badge/?version=latest)](http://59ers-testing-and-documentation.readthedocs.io/en/latest/?badge=latest)
 
 __Team:__ 59ers
 
 __Team Members:__ Harvey Shi (@rvshi), Edward Liang (@edl11), Michelle Wei (@michellewei04)
+
+__Documentation:__ [readthedocs.org](http://59ers-testing-and-documentation.readthedocs.io/en/latest/)
--- test_max_diff.py
+++ test_max_diff.py
@@ -1,6 +1,15 @@
+import pytest
+
+
 def test_max_diff():
     from list_ops import get_max_diff
     input_list = ([1, 1, 3], [-1, 3, -16], [5, 5, 5, 5, 5])
     output_results = (2, 4, 0)
     for i, nums in enumerate(input_list):
         assert get_max_diff(nums) == output_results[i]
+    error_input_list = ([1, 2, 3.32], [4, 'GTHC', 2, 333],
+                        [85, -23, -2222222222222], [33, 123123123, -2])
+    error_output_list = (TypeError, TypeError, ValueError, ValueError)
+    for i, nums in enumerate(error_input_list):
+        with pytest.raises(error_output_list[i]):
+            get_max_diff(nums)
