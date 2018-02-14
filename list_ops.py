04501e3b08757b1aabe6164a95a794d006e08b67
Michelle Wei <michellewei12345@gmail.com>
2018-02-13 21:22:10 -0500

    Add logging v1

--- list_ops.py
+++ list_ops.py
@@ -1,12 +1,13 @@
 """
 Module for performing several basic operations on lists of integers.
 """
-#import logging
-#logging.basicConfig(filename='test.log', level=logging.DEBUG)
+import logging
 
+logging.basicConfig(filename='test.log', filemode='w', level=logging.DEBUG)
+logger = logging.getLogger()
+logger.info("Start logging")
 
 def get_sum(input_list):
-    #logging.info('Calculating sum of the list')
     """ Returns the sum of a list
 
     :param input_list: list of n integers between -9,000 and 9,000
@@ -15,19 +16,22 @@ def get_sum(input_list):
     :raises TypeError: Input elements must be integers
     :raises ValueError: All input elements must be between -9,000 and 9,000 (inclusive)
     """
-    #logging.debug('Input list: %s', str(input_list))
-    # try:
-    #     check_inputs(input_list)
-    # except TypeError:
-    #     logging.warning("Input is the wrong type, should be list of integers")
-    # except ValueError:
-    #     logging.warning("Input values out of range, should be be between -9,000 and 9,000")
-    #
+    logging.basicConfig(filename='test.log', filemode='w', level=logging.DEBUG)
+    logging.info('Calculating sum of the list')
+    check_inputs(input_list)
+    logging.debug('Input list: %s', str(input_list))
+    try:
+        check_inputs(input_list)
+    except TypeError:
+        logging.warning("TypeError in get_sum")
+    except ValueError:
+        logging.warning("ValueError in get_sum")
+
     return sum(input_list)
 
 
 def get_min_max(input_list):
-    # logging.info('Obtaining min and max of list')
+    logging.info('Obtaining min and max of list')
     """ Returns min and max in a list
 
     :param input_list: (int) list to get min and max of
@@ -36,21 +40,21 @@ def get_min_max(input_list):
     :raises TypeError: Input elements must be integers
     :raises ValueError: All input elements must be between -9,000 and 9,000 (inclusive)
     """
-    # logging.debug('Input list: %s', str(input_list))
-    # try:
-    #     check_inputs(input_list)
-    # except TypeError:
-    #     logging.warning("Input is the wrong type, should be list of integers")
-    # except ValueError:
-    #     logging.warning("Input values out of range, should be be between -9,000 and 9,000")
-
+    logging.debug('Input list: %s', str(input_list))
+    try:
+        check_inputs(input_list)
+    except TypeError:
+        logging.warning("TypeError in get_min_max")
+    except ValueError:
+        logging.warning("ValueError in get_min_max")
+    check_inputs(input_list)
     np = import_modules()
     min_max = (np.amin(input_list), np.amax(input_list))
     return min_max
 
 
 def get_max_diff(input_list):
-    # logging.info('Calculating maximum difference in the list')
+    logging.info('Calculating maximum difference in the list')
     """ Returns maximum difference between consecutive elements in input list
     
     :param input_list: list of n integers between -9,000 and 9,000
@@ -60,14 +64,14 @@ def get_max_diff(input_list):
     :raises TypeError: Input elements must be integers
     :raises ValueError: All input elements must be between -9,000 and 9,000 (inclusive)
     """
-    # logging.debug('Input list: %s', str(input_list))
-    # try:
-    #     check_inputs(input_list)
-    # except TypeError:
-    #     logging.warning("Input is the wrong type, should be list of integers")
-    # except ValueError:
-    #     logging.warning("Input values out of range, should be between -9,000 and 9,000")
-
+    logging.debug('Input list: %s', str(input_list))
+    try:
+        check_inputs(input_list)
+    except TypeError:
+        logging.warning("TypeError in get_max_diff")
+    except ValueError:
+        logging.warning("ValueError in get_max_diff")
+    check_inputs(input_list)
     np = import_modules()
     diff_arr = np.diff(input_list)
     max_diff = np.max(diff_arr)
@@ -84,7 +88,7 @@ def import_modules():
     except (ModuleNotFoundError, ImportError) as error:
         raise ImportError(
             '''Could not import numpy. Please make sure to have the package installed''')
-    # logging.info('Imported numpy module')
+    logging.info('Imported numpy module')
     return np
 
 
